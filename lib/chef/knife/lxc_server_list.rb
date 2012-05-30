99881f397094cee4039c9be78ec3c175f15c9489
Lukasz Kaniowski <lukasz.kaniowski@gmail.com>
2012-05-30 17:13:54 +0200

    formatting lxc server list

--- lib/chef/knife/lxc_server_list.rb
+++ lib/chef/knife/lxc_server_list.rb
@@ -12,16 +12,21 @@ module KnifeLxc
     def run
       puts "Lxc containers list"
       containers = `lxc-ls`.split.uniq!
+      server_list = [
+        ui.color('Name', :bold),
+          ui.color('Ip', :bold)
+      ]
       containers.each do |container|
-        ip = get_ip container
-        puts "Name: #{container}, Ip: #{ip}"
+        server_list << container
+        server_list << get_ip(container)
       end
+      puts ui.list(server_list, :uneven_columns_across, 2)
     end
 
     private
     def get_ip(container)
       config_path = find_config(container)
-      File.read( config_path).split("\n").each do |row|
+      File.read(config_path).split("\n").each do |row|
         if row =~ /ipv4/
           ip = row.split("=").last
           ip.strip!
