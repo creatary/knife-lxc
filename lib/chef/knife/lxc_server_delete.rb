b4757ebe5f03e56af30289b2d349a69581c1f127
Lukasz Kaniowski <lukasz.kaniowski@gmail.com>
2012-06-01 18:27:47 +0200

    banner fix

--- lib/chef/knife/lxc_server_delete.rb
+++ lib/chef/knife/lxc_server_delete.rb
@@ -6,7 +6,7 @@ module KnifeLxc
   class LxcServerDelete < Chef::Knife
     include Toft
 
-    banner "knife lxc server delete -N NODE"
+    banner "knife lxc server delete -N NAME"
 
     option :node_name,
       :short => "-N NAME",
